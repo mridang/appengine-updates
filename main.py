a6d694ff8b84b96a1d79b6c14a9d5e3c18765eb0
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2014-04-22 12:20:51 +0300

    Fixed the route.

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ import webapp2
 #trailing slahs
 
 app = webapp2.WSGIApplication([
-    webapp2.Route(r'/<uniqid:([a-zA-Z0-9]{32})>/<android:([0-9]{2})>/<device:([a-zA-Z0-9]{4,12})>/',      handler='handlers.Updates'),
+    webapp2.Route(r'/<uniqid:([a-zA-Z0-9]{32})>/<android:([0-9]{2})>/<device:([a-zA-Z0-9]{2,12})>/',      handler='handlers.Updates'),
 ], debug=True)
